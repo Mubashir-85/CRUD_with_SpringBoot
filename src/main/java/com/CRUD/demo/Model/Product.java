e416123c5ee88752ded45f92345765c382e971e9
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-04 16:25:35 +0530

    Product method added

--- src/main/java/com/CRUD/demo/Model/Product.java
+++ src/main/java/com/CRUD/demo/Model/Product.java
@@ -1,6 +1,44 @@
 package com.CRUD.demo.Model;
 
-public class Product {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Product {
+    private int prodId;
+    private String prodName;
+    private int prodPrice;
+
+    public Product(){
+
+    }
+    public Product(int prodId, String prodName, int prodPrice){
+        this.prodId = prodId;
+        this.prodName = prodName;
+        this.prodPrice = prodPrice;
+    }
+
+    public int getProdId(){
+        return prodId;
+    }
+
+    public void setProdId(int id){
+        this.prodId = id;
+    }
+
+    public String getProdName(){
+        return prodName;
+    }
+
+    public void  setProdName(String name){
+        this.prodName = name;
+    }
+
+    public int getProdPrice(){
+        return prodPrice;
+    }
+
+    public void setProdPrice(int price){
+        this.prodPrice = price;
+    }
 
 }
