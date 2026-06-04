266e6943881392d90aa636bc83ddd2aae10a9e12
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-04 16:44:49 +0530

    Product Controller added

--- src/main/java/com/CRUD/demo/Controller/ProductController.java
+++ src/main/java/com/CRUD/demo/Controller/ProductController.java
@@ -1,4 +1,20 @@
 package com.CRUD.demo.Controller;
 
+import com.CRUD.demo.Model.Product;
+import com.CRUD.demo.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+    @Autowired
+    ProductService service;
+
+    @RequestMapping("/products")
+    public List<Product> getProducts() {
+        return service.getProducts();
+    }
 }
