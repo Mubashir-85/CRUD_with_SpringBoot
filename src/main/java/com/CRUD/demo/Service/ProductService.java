0e608b4d57e35bf9ce837b589708a518a40e48e9
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-04 16:33:45 +0530

    Product servise method added

--- src/main/java/com/CRUD/demo/Service/ProductService.java
+++ src/main/java/com/CRUD/demo/Service/ProductService.java
@@ -1,4 +1,20 @@
 package com.CRUD.demo.Service;
 
+import com.CRUD.demo.Model.Product;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class ProductService {
+    List<Product> products = Arrays.asList(
+            new Product(101,"Samsung", 40000),
+            new Product(102,"Oppo", 10000),
+            new Product(103,"Apple", 30000)
+    );
+    public List<Product> getProducts(){
+        return products;
+    }
 }
